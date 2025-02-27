9abbe61d22952aa7145b8f932f1f425df304c2e5
Shrutkeerti200 <sangolkar.s@northeastern.edu>
2025-02-27 10:28:40 -0500

    error resolved

--- main.tf
+++ main.tf
@@ -162,26 +162,6 @@ resource "aws_instance" "app_instance" {
   }
 }
 
-resource "google_compute_instance" "vm_instance" {
-  project      = var.gcp_project_id
-  name         = var.instance_name
-  machine_type = var.machine_type
-  zone         = var.zone
-
-  boot_disk {
-    initialize_params {
-      image = var.image
-    }
-  }
-
-  network_interface {
-    network = var.network
-    access_config {
-      // Ephemeral IP
-    }
-  }
-}
-
 resource "google_compute_instance" "vm_instance"{
   project      = var.gcp_project_id
   name         = var.instance_name
