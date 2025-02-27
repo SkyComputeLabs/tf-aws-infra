1e41ac8c77d4c50ea2ec080855306992662140d4
Shrutkeerti200 <sangolkar.s@northeastern.edu>
2025-02-27 10:18:05 -0500

    Assignment 4 - packer and terraform

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,14 +1,32 @@
-aws_region  = "us-east-1"
-aws_profile = "dev"
+aws_region = "us-east-1"
+#aws_profile = "User1"
 
-vpc_cidr = "10.0.0.0/16"
+vpc_cidr = "10.1.0.0/16"
 
-public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-private_subnet_cidrs = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+public_subnet_cidrs  = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+private_subnet_cidrs = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
 
 availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
 
-vpc_name = "vpc0"
+vpc_name        = "vpc1"
 igw_name        = "main-igw"
 public_rt_name  = "main-public-rt"
 private_rt_name = "main-private-rt"
+
+# New variables
+
+ami_id        = "ami-02530d9cb2af8cb77"
+instance_type = "t2.small"
+
+app_port = 8080
+
+root_volume_size                  = 25
+root_volume_type                  = "gp2"
+root_volume_delete_on_termination = true
+
+gcp_project_id = "csye6225-dev-451900"
+instance_name  = "csye6225-dev-instance"
+region         = "us-central1"
+machine_type   = "n1-standard-1"
+zone           = "us-central1-a"
+image          = "projects/debian-cloud/global/images/debian-10-buster-v20220406"
--- variables.tf
+++ variables.tf
@@ -43,7 +43,81 @@ variable "private_rt_name" {
   description = "Name for the private route table"
 }
 
-variable "vpc_name"{
-  type = string
+variable "vpc_name" {
+  type        = string
   description = "Name for VPC"
+}
+
+# New variables
+
+variable "ami_id" {
+  type        = string
+  description = "AMI ID for the EC2 instance"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Instance type for the EC2 instance"
+}
+
+variable "app_port" {
+  type        = number
+  description = "Port on which the application runs"
+}
+
+variable "root_volume_size" {
+  type        = number
+  description = "Size of the root volume in GB"
+  default     = 25
+}
+
+variable "root_volume_type" {
+  type        = string
+  description = "Type of the root volume"
+  default     = "gp2"
+}
+
+variable "root_volume_delete_on_termination" {
+  type        = bool
+  description = "Whether to delete the root volume on instance termination"
+  default     = true
+}
+
+variable "region" {
+  type        = string
+  description = "Region for the GCE instance"
+  default     = "us-central1"
+}
+
+variable "gcp_project_id" {
+  type        = string
+  description = "The GCP project ID"
+}
+
+variable "instance_name" {
+  type        = string
+  description = "Name of the GCE instance"
+}
+
+variable "machine_type" {
+  type        = string
+  description = "Machine type for the GCE instance"
+  default     = "n1-standard-1"
+}
+
+variable "zone" {
+  type        = string
+  description = "Zone for the GCE instance"
+  default     = "us-central1-a"
+}
+
+variable "image" {
+  type        = string
+  description = "Boot disk image for the GCE instance"
+}
+
+variable "network" {
+  type        = string
+  description = "Network for the GCE instance"
+  default     = "default"
 }
