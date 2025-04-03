89d37bd7028d6c58289ee760778b976c1c9c6537
Shrutkeerti200 <sangolkar.s@northeastern.edu>
2025-04-03 10:20:07 -0400

    Assignment 7

--- main.tf
+++ main.tf
@@ -11,22 +11,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-#resource "aws_route53_zone" "dev" {
-#  name = "dev.shrutkeerti.me"
-#}
-
-#resource "aws_route53_zone" "demo" {
-#  name = "demo.shrutkeerti.me"
-#}
-
-#resource "aws_route53_record" "dev_a" {
-#  zone_id = aws_route53_zone.dev.zone_id
-#  name    = "dev.shrutkeerti.me"
-#  type    = "A"
-#  ttl     = "300"
-#  records = [aws_instance.app_instance.public_ip]
-#}
-
 # Random suffix for unique resource names
 resource "random_string" "suffix" {
   length  = 6
@@ -111,39 +95,56 @@ resource "aws_security_group" "app_sg" {
   description = "Application security group"
   vpc_id      = aws_vpc.main.id
 
+  #ingress {
+  #  description = "HTTP"
+  #  from_port   = 80
+  #  to_port     = 80
+  #  protocol    = "tcp"
+  #  cidr_blocks = ["0.0.0.0/0"]
+  #}
+
+  #ingress {
+  #  description = "HTTPS"
+  #  from_port   = 443
+  #  to_port     = 443
+  #  protocol    = "tcp"
+  #  cidr_blocks = ["0.0.0.0/0"]
+  #}
+
+  #ingress {
+  #  description = "SSH"
+  #  from_port   = 22
+  #  to_port     = 22
+  #  protocol    = "tcp"
+  #  cidr_blocks = ["0.0.0.0/0"]
+  #}
+
+  #ingress {
+  #  description = "Spring Boot Application Port"
+  #  from_port   = 8080
+  #  to_port     = 8080
+  #  protocol    = "tcp"
+  #  cidr_blocks = ["0.0.0.0/0"]
+  #}
+
   ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description     = "Allow traffic from Load Balancer on application port"
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.load_balancer_sg.id]
   }
 
   ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "SSH"
+    description = "Allow SSH access for management"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Spring Boot Application Port"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.public_ip_address]
   }
 
   egress {
+    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -176,6 +177,40 @@ resource "aws_security_group" "db_sg" {
 
   tags = {
     Name = "db-sg-${random_string.suffix.result}"
+  }
+}
+
+resource "aws_security_group" "load_balancer_sg" {
+  name        = "load-balancer-sg-${random_string.suffix.result}"
+  description = "Security group for Load Balancer"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Allow HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "load-balancer-sg-${random_string.suffix.result}"
   }
 }
 
@@ -358,16 +393,122 @@ resource "aws_iam_role_policy_attachment" "attach_rds_access" {
   policy_arn = aws_iam_policy.rds_access.arn
 }
 
+resource "aws_route53_zone" "root" {
+  name = "webapp-cloud-metrics.com"
+}
+
+# Alias Record for dev.webapp-cloud-metrics.com
+resource "aws_route53_record" "dev_alias" {
+  zone_id = aws_route53_zone.root.zone_id
+  name    = "dev.webapp-cloud-metrics.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.web_app_lb.dns_name
+    zone_id                = aws_lb.web_app_lb.zone_id
+    evaluate_target_health = true
+  }
+}
+
+# Alias Record for demo.webapp-cloud-metrics.com
+resource "aws_route53_record" "demo_alias" {
+  zone_id = aws_route53_zone.root.zone_id
+  name    = "demo.webapp-cloud-metrics.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.web_app_lb.dns_name
+    zone_id                = aws_lb.web_app_lb.zone_id
+    evaluate_target_health = true
+  }
+}
+
 # EC2 instance configuration
-resource "aws_instance" "app_instance" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.app_sg.id]
-  subnet_id              = aws_subnet.public[0].id
-  iam_instance_profile   = aws_iam_instance_profile.webapp_instance_profile.name
+#resource "aws_instance" "app_instance" {
+#  ami                    = var.ami_id
+#  instance_type          = var.instance_type
+#  vpc_security_group_ids = [aws_security_group.app_sg.id]
+#  subnet_id              = aws_subnet.public[0].id
+#  iam_instance_profile   = aws_iam_instance_profile.webapp_instance_profile.name
+#
+#
+#  user_data = <<-EOF
+#              #!/bin/bash
+#              echo "DB_HOST=${aws_db_instance.postgres_db.endpoint}" >> /etc/environment
+#              echo "DB_PORT=5432" >> /etc/environment
+#              echo "DB_NAME=${aws_db_instance.postgres_db.db_name}" >> /etc/environment
+#              echo "DB_USERNAME=${aws_db_instance.postgres_db.username}" >> /etc/environment
+#              echo "DB_PASSWORD=${aws_db_instance.postgres_db.password}" >> /etc/environment
+#              echo "S3_BUCKET_NAME=${aws_s3_bucket.webapp_bucket.bucket}" >> /etc/environment
+#              echo "AWS_REGION=${var.aws_region}" >> /etc/environment
+#              echo "APP_PORT=${var.app_port}" >> /etc/environment
+#
+#              # Configure and start CloudWatch Agent
+#              sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+#                -a fetch-config \
+#                -m ec2 \
+#                -c file:/opt/aws/amazon-cloudwatch-agent/etc/cloudwatch-agent-config.json \ 
+#                -s
+#
+#              if ! systemctl is-active --quiet amazon-cloudwatch-agent; then
+#                echo "CloudWatch Agent failed to start" >&2
+#                exit 1
+#              fi  
+#
+#              sudo systemctl enable amazon-cloudwatch-agent
+#              sudo systemctl start amazon-cloudwatch-agent
+#
+#              sudo systemctl restart csye6225.service
+#
+#              EOF
+#
+#  root_block_device {
+#    volume_size           = var.root_volume_size
+#    volume_type           = var.root_volume_type
+#    delete_on_termination = var.root_volume_delete_on_termination
+#  }
+#
+#  disable_api_termination = false
+#
+#  tags = {
+#    Name = "web-application-instance"
+#  }
+#}
 
+# EC2 instance Launch Template
+resource "aws_launch_template" "web_app_template" {
+  name        = "csye6225_asg"
+  description = "Launch template for web application"
 
-  user_data = <<-EOF
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+
+  key_name = var.key_name
+
+  # Attach IAM instance profile for permissions (e.g., S3, CloudWatch)
+  iam_instance_profile {
+    name = aws_iam_instance_profile.webapp_instance_profile.name
+  }
+
+  # Attach the application security group
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.app_sg.id]
+    subnet_id                   = aws_subnet.public[0].id
+  }
+
+  # Root block device configuration
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size           = var.root_volume_size
+      volume_type           = var.root_volume_type
+      delete_on_termination = var.root_volume_delete_on_termination
+    }
+  }
+
+  # User Data Script 
+  user_data = base64encode(<<-EOF
               #!/bin/bash
               echo "DB_HOST=${aws_db_instance.postgres_db.endpoint}" >> /etc/environment
               echo "DB_PORT=5432" >> /etc/environment
@@ -382,23 +523,118 @@ resource "aws_instance" "app_instance" {
               sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
                 -a fetch-config \
                 -m ec2 \
-                -c file:/opt/aws/amazon-cloudwatch-agent/etc/cloudwatch-agent-config.json \ 
+                -c file:/opt/aws/amazon-cloudwatch-agent/etc/cloudwatch-agent-config.json \
                 -s
+
+              if ! systemctl is-active --quiet amazon-cloudwatch-agent; then
+                echo "CloudWatch Agent failed to start" >&2
+                exit 1
+              fi  
+
+              sudo systemctl enable amazon-cloudwatch-agent
+              sudo systemctl start amazon-cloudwatch-agent
+
+              sudo systemctl restart csye6225.service 
               EOF
-
-  root_block_device {
-    volume_size           = var.root_volume_size
-    volume_type           = var.root_volume_type
-    delete_on_termination = var.root_volume_delete_on_termination
-  }
-
-  disable_api_termination = false
+              )
 
   tags = {
-    Name = "web-application-instance"
+    Name = "WebAppInstance"
   }
 }
 
+# Auto Scaling Groups
+resource "aws_autoscaling_group" "web_app_asg" {
+  desired_capacity = var.desired_capacity
+  min_size         = var.min_size
+  max_size         = var.max_size
+  default_cooldown = 60
+
+  launch_template {
+    id      = aws_launch_template.web_app_template.id
+    version = "$Latest"
+  }
+
+  vpc_zone_identifier = aws_subnet.public[*].id
+
+  health_check_type         = "EC2"
+  health_check_grace_period = 300
+
+  tag {
+    key                 = "Name"
+    value               = "WebAppInstance"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    ignore_changes = [desired_capacity]
+  }
+}
+
+resource "aws_autoscaling_policy" "scale_up_policy" {
+  name                   = "scale_up_policy"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  autoscaling_group_name = aws_autoscaling_group.web_app_asg.name
+
+  metric_aggregation_type = "Average"
+}
+
+resource "aws_autoscaling_policy" "scale_down_policy" {
+  name                   = "scale_down_policy"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  autoscaling_group_name = aws_autoscaling_group.web_app_asg.name
+
+  metric_aggregation_type = "Average"
+}
+
+# Application Load Balancer
+resource "aws_lb" "web_app_lb" {
+  name               = "web-app-alb"
+  load_balancer_type = "application"
+
+  security_groups = [aws_security_group.load_balancer_sg.id]
+  subnets         = aws_subnet.public[*].id
+
+  enable_deletion_protection = false
+  tags = {
+    Name = "WebAppALB"
+  }
+}
+
+resource "aws_lb_target_group" "web_app_tg" {
+  name = "web-app-tg"
+
+  port     = var.app_port
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  target_type = "instance"
+
+  health_check {
+    path                = "/healthz"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    protocol            = "HTTP"
+    port                = var.app_port
+  }
+
+  tags = { Name = "WebAppTargetGroup" }
+}
+
+resource "aws_lb_listener" "web_app_listener" {
+  load_balancer_arn = aws_lb.web_app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_app_tg.arn
+  }
+}
 
 # RDS Configuration
 resource "aws_db_subnet_group" "db_subnet_group" {
@@ -501,7 +737,7 @@ resource "aws_iam_policy" "custom_cloudwatch_policy" {
           "logs:CreateLogStream",
           "logs:PutLogEvents",
           "logs:DescribeLogStreams"
-        ]
+        ],
         Resource = "*"
       }
     ]
